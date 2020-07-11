eff9261a20427fd6bd6894d0311f19035dd7b507
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-11 17:30:03 -0600

    create rake task

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20200711200055) do
 
   create_table "transactions", force: :cascade do |t|
     t.bigint "invoice_id"
-    t.integer "credit_card_number"
+    t.bigint "credit_card_number"
     t.float "credit_card_expiration_date"
     t.string "result"
     t.datetime "created_at", null: false
