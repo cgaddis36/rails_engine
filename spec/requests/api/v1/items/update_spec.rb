7b849050c58a72dba6ded272603b6ce2f683ccb5
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-13 17:12:42 -0600

    spec creation for Item update and show

---
+++ spec/requests/api/v1/items/update_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "API request" do
+  it "update a single item" do
+    merchant = create(:merchant)
+    item = create(:item, merchant_id: merchant.id)
+
+    patch "/api/v1/items/#{item.id}", params: { name: Faker::Games::Zelda.item,
+                                                    description: Faker::TvShows::GameOfThrones.quote,
+                                                    unit_price: rand(11.2...76.9),
+                                                    merchant_id: merchant.id
+                                                    }
+
+    parsed_data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(parsed_data[:id].to_i).to eq(item.id)
+    expect(parsed_data[:attributes][:name]).to_not eq(item.name)
+  end
+end
