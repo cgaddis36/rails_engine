ce2b686f750f52364036b9e0a64d2c0c5d6d87a7
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-13 17:11:52 -0600

    add blank lines

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,17 @@ Rails.application.routes.draw do
       post '/merchants', to: 'merchants#create', param: 'name'
       patch '/merchants/:id', to: 'merchants#update', param: 'name'
       delete '/merchants/:id', to: 'merchants#destroy'
+
       namespace :merchants do
         get '/:id/items', to: 'search#show'
       end
+
       get '/items', to: 'items#index'
       get '/items/:id', to: 'items#show'
       post '/items', to: 'items#create', param: 'name'
       patch '/items/:id', to: 'items#update', param: 'name'
       delete '/items/:id', to: 'items#destroy'
+
       namespace :items do
         get '/:id/merchant', to: 'search#show'
       end
