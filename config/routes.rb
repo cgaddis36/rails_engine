8986c368e14fe2eb9b163282582d9b4398740f75
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-13 14:32:25 -0600

    folder reconfig

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/merchants', to: 'merchants/search#index'
-      get '/merchants/:id', to: 'merchants/search#show'
-      post '/merchants', to: 'merchants/search#create', param: 'name'
-      patch '/merchants/:id', to: 'merchants/search#update', param: 'name'
-      delete '/merchants/:id', to: 'merchants/search#destroy'
+      get '/merchants', to: 'merchants#index'
+      get '/merchants/:id', to: 'merchants#show'
+      post '/merchants', to: 'merchants#create', param: 'name'
+      patch '/merchants/:id', to: 'merchants#update', param: 'name'
+      delete '/merchants/:id', to: 'merchants#destroy'
     end
   end
 end
