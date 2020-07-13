425372041131937901e85420bcebaa786a48a13a
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-13 14:05:16 -0600

    add params to post route for new merchant

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       get '/merchants', to: 'merchants/search#index'
       get '/merchants/:id', to: 'merchants/search#show'
+      post '/merchants', to: 'merchants/search#create', param: 'name'
     end
   end
 end
