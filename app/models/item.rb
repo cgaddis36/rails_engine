361b7712ee9f6d0c80462bd3d0c72956d3304015
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-13 20:43:07 -0600

    add helper methods for find endpoints

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,12 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
+
+  def self.find_item(item_name)
+    where('lower(name) like ?', "%" + item_name.downcase + "%").first
+  end
+
+  def self.find_items(item_name)
+    where('lower(name) like ?', "%" + item_name.downcase + "%")
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,4 +5,8 @@ class Merchant < ApplicationRecord
   def self.find_merchant(search_name)
     Merchant.where('lower(name) like ?', "%" + search_name.downcase + "%").first
   end
+
+  def self.find_merchants(search_name)
+    Merchant.where('lower(name) like ?', "%" + search_name.downcase + "%")
+  end
 end
