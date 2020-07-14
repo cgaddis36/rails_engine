69556e51ed554353486a0b5d83a59dcfd8faf75c
Chase.Gaddis <cgaddis36@gmail.com>
2020-07-13 20:43:16 -0600

    spec config

--- spec/requests/api/v1/merchants/find_spec.rb
+++ spec/requests/api/v1/merchants/find_spec.rb
@@ -12,4 +12,16 @@ describe "Api Find endpoints" do
 
     expect(parsed_data[:attributes][:name]).to eq(merchant.name)
   end
+  it "can find the all merchants for that find_all parameter" do
+    merchant = Merchant.create!(name: "Turing")
+    Merchant.create!(name: "Ring World")
+    Merchant.create!(name: "Notterham Wolves")
+
+    get "/api/v1/merchants/find_all?name=ring"
+
+    parsed_data = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(parsed_data.count).to eq(2)
+    expect(parsed_data.count).to_not eq(Merchant.all.count)
+  end
 end
